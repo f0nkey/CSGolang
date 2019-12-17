6d53bf5480b4511315ac71969e6904a91978b389
f0nkey <32034058+f0nkey@users.noreply.github.com>
2019-12-17 00:22:04 -0600

    enabling drawing

--- main.go
+++ main.go
@@ -2,47 +2,50 @@ package main
 
 import (
 	"CSGolang/feature"
+	"CSGolang/memory"
+	"CSGolang/offset"
+	"CSGolang/render"
+	"CSGolang/types"
+	"log"
 	"time"
 )
 
 var enableVisuals = true
 
-//var windowSize = types.GetWindowSize()
+var windowSize = types.GetWindowSize()
 
 func main() {
-
 	config := feature.InitConfig()
 	go feature.WatchConfig(config)
 	go feature.RunConfigEndpoint(config) //localhost:9991
 	go feature.ServeWebGUI() //localhost:8085
 	//
-	//offset.InitOffsets()
-	//memEditor := memory.NewEditor("csgo.exe")
-	//
-	//ps := feature.NewPlayerStore(12)
-	//ps.UpdateAllPlayers(memEditor)
-	//
-	//go feature.BHop(memEditor, config.Toggles.Bhop)
-	//
-	//gw := render.NewDrawingOverlay("Counter-Strike: Global Offensive", "F0nkOverlay",
-	//	func(canvas *render.DrawingCanvas) {
-	//
-	//		ps.UpdateAllPlayers(memEditor)
-	//		vm, err := feature.GetViewMatrix(memEditor)
-	//		if err != nil {
-	//			log.Println(err)
-	//		}
-	//
-	//		if config.Toggles.Skeleton {
-	//			feature.DrawBones(canvas, ps, vm, windowSize, config.ColorModes.Skeleton)
-	//		}
-	//
-	//		if config.Toggles.Name {
-	//			feature.DrawNames(canvas, ps, vm, config.ColorModes.Name)
-	//		}
-	//
-	//	})
-	//gw.RunGL()
+	offset.InitOffsets()
+	memEditor := memory.NewEditor("csgo.exe")
+
+	ps := feature.NewPlayerStore(12)
+	ps.UpdateAllPlayers(memEditor)
+
+	go feature.BHop(memEditor, config.Toggles.Bhop)
+
+	gw := render.NewDrawingOverlay("Counter-Strike: Global Offensive", "F0nkOverlay",
+		func(canvas *render.DrawingCanvas) {
+
+			ps.UpdateAllPlayers(memEditor)
+			vm, err := feature.GetViewMatrix(memEditor)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if config.Toggles.Skeleton {
+				feature.DrawBones(canvas, ps, vm, windowSize, config.ColorModes.Skeleton)
+			}
+
+			if config.Toggles.Name {
+				feature.DrawNames(canvas, ps, vm, config.ColorModes.Name)
+			}
+		})
+	gw.RunGL()
 
 	for {
 		time.Sleep(time.Millisecond * 5)
--- types/types.go
+++ types/types.go
@@ -1,15 +1,20 @@
 package types
 
+import (
+	"CSGolang/memory"
+	"github.com/AllenDang/w32"
+)
+
 type WindowRect struct {
 	Width, Height int32
 }
 
 func GetWindowSize() WindowRect {
-	//w, err := memory.FindWindow("Counter-Strike: Global Offensive")
-	//if err != nil {
-	//	panic("Did not find Counter-Strike: Global Offensive window. Is CS:GO is open?")
-	//}
-	//rect := w32.GetWindowRect(w32.HWND(w))
-	//return WindowRect{rect.Right - rect.Left, rect.Bottom - rect.Top}
+	w, err := memory.FindWindow("Counter-Strike: Global Offensive")
+	if err != nil {
+		panic("Did not find Counter-Strike: Global Offensive window. Is CS:GO is open?")
+	}
+	rect := w32.GetWindowRect(w32.HWND(w))
+	return WindowRect{rect.Right - rect.Left, rect.Bottom - rect.Top}
 	return WindowRect{}
 }
